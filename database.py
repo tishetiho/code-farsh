655f1b5c0fdaa691520dfd4d97d0ab364d68077f
tishetiho <moteoAZ@yandex.ru>
2026-03-23 23:03:18 +0700

    Update database.py

--- database.py
+++ database.py
@@ -31,13 +31,13 @@ class Database:
         """)
         await cls.conn.commit()
         async def init(self):
-        await self.conn.execute("""CREATE TABLE IF NOT EXISTS items 
+            await self.conn.execute("""CREATE TABLE IF NOT EXISTS items 
         (id INTEGER PRIMARY KEY, name TEXT, price INTEGER)""")
     
-        await self.conn.execute("""CREATE TABLE IF NOT EXISTS p2p_market 
+            await self.conn.execute("""CREATE TABLE IF NOT EXISTS p2p_market 
         (id INTEGER PRIMARY KEY, seller_id INTEGER, item_name TEXT, price INTEGER, status TEXT)""")
     
-        await self.conn.execute("""CREATE TABLE IF NOT EXISTS mail 
+            await self.conn.execute("""CREATE TABLE IF NOT EXISTS mail 
         (id INTEGER PRIMARY KEY, user_id INTEGER, sender TEXT, text TEXT, is_read INTEGER DEFAULT 0)""")
         await self.conn.commit()
 
