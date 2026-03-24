0678c5bad8a3eff9f8bfcb46f0fc53ccb5ba8c99
tishetiho <moteoAZ@yandex.ru>
2026-03-24 08:26:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio, logging
+import config
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage # Для хранения данных в памяти
